dfca5e53e65f821e6d962ea9046d2ee29e4701c2
Renato Leite <hr2.renato@gmail.com>
2012-06-29 19:43:16 -0300

    Restauração

---
+++ index.php
@@ -0,0 +1,16 @@
+<?	
+	require('./inc/class/base.class.php');
+	require('./inc/class/util.class.php');
+	
+	$RoBase = new Base();
+	
+	$RoBase->set('tabela',"patrocinador");
+	$RoBase->set('fields',"id,nome,link");
+	$RoBase->set('where',"1=1");				
+	$rs = $RoBase->query();	
+				
+	while($result = mysql_fetch_array($rs)){                                                 		
+		echo $result['link']."<br>";
+		
+	}	
+?>
